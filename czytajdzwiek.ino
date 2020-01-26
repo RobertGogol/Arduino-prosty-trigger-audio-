2ea58b960a79260300f5faa30b2822ca1b317d61
Robert Gogol Elektroakustyk <robert.gogol@gmail.com>
2020-01-26 18:05:36 +0100

    Update czytajdzwiek.ino

--- czytajdzwiek.ino
+++ czytajdzwiek.ino
@@ -1,12 +1,12 @@
 /*
-Robert Gogol 26.01.2020 Poznań
+by Robert Gogol 26.01.2020 | KOLORKING MUZYCZNY Poznań
 PROSTY TRIGGER AUDIO
 1.podawany sygnał dostosowac korektorem graficznym / filtrem tak aby uwypuklic 
 pasmo, ktore ma triggerować uklad
 2.sygnal podac przez kondensator okolo 100uF na dzielnk napiecia 100k/100k rozpiety miedzy GND i A0
 3.czułość ukladu to odstępsto od zera wejścia A0, w zakresie od polowy 
 rozdzielczosci przetwornika do, jak mi wyszlo z testow - odchylki okolo +/- 100 
-czyli 1023/2=ca.512  np. 400-512 lub 512-612
+czyli 1023/2=ca.512  np. 400-512 w dol lub 512-612 w gore
 >>490 to czulosc bardzo duza
 >>470 to czulosc sredna
 >>400 to czulosc bardzo niska 
